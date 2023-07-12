23cfb80261671c8881d8640aafcc74b159656e6c
Alex L <alexlitvak90@gmail.com>
2023-07-13 00:01:45 +0300

    sections scroll and styling

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,13 @@
 function animateSlidingElements({ section_headers, section_divs }) {
     const intersectionObserver = new IntersectionObserver(entries => {
         entries.forEach(entry => {
-            if (entry.intersectionRatio > 0.2) {
+            if (entry.intersectionRatio > 0.3) {
                 entry.target.classList.add('slider');
             } else {
                 entry.target.classList.remove('slider');
             }
         });
-    }, {threshold: [.1, .25, .35, .5, .65, .75, .9]});
+    }, {threshold: [.1, .2, .3, .4, .5, .6, .7, .8, .9]});
     section_headers.forEach(header => {
         intersectionObserver.observe(header);
     });
